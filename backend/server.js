cbfa34e6e1be18cdffef969eb0574bff4aec93f7
Kevin Rangel <thatguykev@Kevins-MacBook-Air.local>
2023-02-01 10:30:30 -0600

    Launch test 01

--- backend/server.js
+++ backend/server.js
@@ -45,5 +45,5 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
+  console.log(`server at http://localhost:${port}`);
 });
